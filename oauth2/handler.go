966f60726e8fc7f8b41639d006ffd48b946cf0da
phongnhat19 <phongnhat19@gmail.com>
2023-02-15 16:09:53 +0700

    hot fix

--- oauth2/handler.go
+++ oauth2/handler.go
@@ -791,7 +791,7 @@ func (h *Handler) introspectOAuth2Token(w http.ResponseWriter, r *http.Request, 
 
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	w.Header().Set("X-Client-ID", resp.GetAccessRequester().GetClient().GetID())
-	w.Header().Set("X-Client-Active", strconv.FormatBool(resp.IsActive()))
+	w.Header().Set("X-Token-Active", strconv.FormatBool(resp.IsActive()))
 
 	if err = json.NewEncoder(w).Encode(&Introspection{
 		Active:            resp.IsActive(),
